90a1bc3ec1931f669ed9d596fc6c3d10a8511e52
lanping <17302010082@fudan.edu.cn>
2018-05-20 21:49:08 +0800

    lab08

--- functions.inc.php
+++ functions.inc.php
@@ -10,6 +10,25 @@ function generateLink($url, $label, $class) {
 
 function outputPostRow($number)  {
     include("travel-data.inc.php");
+    $a='userName'.$number;
+    $userName=$$a;
+    $b='date'.$number;
+    $date=$$b;
+    $c='title'.$number;
+    $title=$$c;
+    $d='excerpt'.$number;
+    $excerpt=$$d;
+    $e='reviewsNum'.$number;
+    $reviewsNum=$$e;
+    $f='reviewsRating'.$number;
+    $reviewsRating=$$f;
+    $g='thumb'.$number;
+    $thumb=$$g;
+    $h='postId'.$number;
+    $postId=$$h;
+    $i='userId'.$number;
+    $userId=$$i;
+    echo'<div class="row"><div class="col-md-4"><a href="post.php?id='.$postId.'" class=""><img src="images/'.$thumb.'" alt='.$title.' class="img-responsive"/></a></div><div class="col-md-8"><h2>'.$title.'</h2><div class="details">Posted by <a href="user.php?id='.$userId.'" class="">'.$userName.'</a><span class="pull-right">'.$date.'</span><p class="ratings">'.constructRating($reviewsRating).$reviewsNum.'Reviews</p></div><p class="excerpt">'.$excerpt.'</p><p><a href="" class="btn btn-primary btn-sm">Read more</a></p></div></div><hr/> ';
 }
 
 /*
